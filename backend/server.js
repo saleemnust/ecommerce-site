9fbb34a041f966421c0c4805605f29687d13c9db
saleemnust <msaleem.bese15seecs@seecs.edu.pk>
2022-03-07 23:49:02 +0500

    Mongo database connection logic added, Product model created.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const app = require("./app");
-const dotenv = require("./config/config.env");
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
 // config
 dotenv.config({path:"backend/config/config.env"});
+// Connecting to db
+connectDatabase();
 app.listen(process.env.PORT, ()=>{
-    console.log(`Server is lsitening on https:/localhost:${process.env.port}`);
+    console.log(`Server is lsitening on https://localhost:${process.env.PORT}`);
 });
